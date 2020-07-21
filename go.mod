da4a6bbce3c4cca4050f323031b6971547309233
bug <2532917800@qq.com>
2020-07-21 10:31:37 +0800

    init

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/livegoplayer/go_session
+
+go 1.14
+
+require (
+	github.com/livegoplayer/go_helper v1.0.40
+	github.com/livegoplayer/go_redis_helper v1.0.1
+)
---
+++ session.go
@@ -0,0 +1,20 @@
+package go_session
+
+import (
+	"time"
+
+	myHelper "github.com/livegoplayer/go_helper"
+	redisHelper "github.com/livegoplayer/go_redis_helper"
+)
+
+func InitRedisSession(host string, port string, password string, db int, pre string) {
+	redisHelper.InitRedisHelper(host, port, password, db, pre)
+}
+
+func CheckUserSession(key string) int64 {
+	return redisHelper.IsNameExits(key)
+}
+
+func SetUserSession(key string, expireTime time.Duration) {
+	redisHelper.SetCacheData(key, myHelper.StringToBytes("1"), expireTime)
+}
